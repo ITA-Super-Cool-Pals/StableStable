2c6ce277d1ddbca3c5c809842dc3f43d7642fcd6
Emythiel <18901520+Emythiel@users.noreply.github.com>
2024-04-24 14:29:02 +0200

    Improved create user - Create User buttons opens a new window for user creation - User is tasked with inputting name, phone, email, password - Once created, database entry added with users email, name and phone

---
+++ app/src/main/java/com/example/stablestable/ui/login/CreateUserWindow.kt
@@ -0,0 +1,111 @@
+package com.example.stablestable.ui.login
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import androidx.lifecycle.viewmodel.compose.viewModel
+
+@Composable
+fun CreateUserWindow(
+    onConfirm: (String, String, String, String) -> Unit,
+    onDismiss: () -> Unit
+) {
+    Dialog(
+        onDismissRequest = onDismiss
+    ) {
+        Box(
+            modifier = Modifier
+                .background(Color.White)
+                .padding(25.dp)
+        ) {
+            val loginViewModel = viewModel<LoginViewModel>()
+
+            Column {
+                Text(
+                    "User Registration",
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    style = MaterialTheme.typography.headlineSmall,
+                    textAlign = TextAlign.Center
+                )
+
+                Spacer(modifier = Modifier.height(16.dp))
+                // Name Field
+                OutlinedTextField(
+                    value = loginViewModel.fullName,
+                    onValueChange = { loginViewModel.fullName = it },
+                    label = { Text("Name") }
+                )
+
+                Spacer(modifier = Modifier.height(8.dp))
+                // Phone Field
+                OutlinedTextField(
+                    value = loginViewModel.phone,
+                    onValueChange = {
+                        if (it.all { char -> char.isDigit() }) {
+                            loginViewModel.phone = it
+                        }
+                    },
+                    label = { Text("Phone") },
+                    keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number)
+                )
+
+                Spacer(modifier = Modifier.height(8.dp))
+                // Email field
+                OutlinedTextField(
+                    value = loginViewModel.email,
+                    onValueChange = { loginViewModel.email = it },
+                    label = { Text("Email") },
+                    keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Email)
+                )
+
+                Spacer(modifier = Modifier.height(8.dp))
+                // Password field
+                OutlinedTextField(
+                    value = loginViewModel.password,
+                    onValueChange = { loginViewModel.password = it },
+                    label = { Text("Password") },
+                    visualTransformation = PasswordVisualTransformation(), // Hide password text
+                    keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Password)
+                )
+
+                Spacer(modifier = Modifier.height(8.dp))
+                Button(
+                    onClick = {
+                        onConfirm(
+                            loginViewModel.fullName,
+                            loginViewModel.phone,
+                            loginViewModel.email,
+                            loginViewModel.password
+                        )
+                        onDismiss()
+                    },
+                    modifier = Modifier
+                        .padding(vertical = 8.dp)
+                        .fillMaxWidth(),
+                    shape = RoundedCornerShape(12.dp)
+                ) {
+                    Text("Create Account")
+                }
+            }
+        }
+    }
+}
