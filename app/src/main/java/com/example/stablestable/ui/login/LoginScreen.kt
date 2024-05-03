66014de486c8c44e5907ac7862b7439fd4afb84f
jose-tl <j.tonnesenlucas@gmail.com>
2024-05-03 13:02:49 +0200

    Moved Create user window

--- app/src/main/java/com/example/stablestable/ui/login/LoginScreen.kt
+++ app/src/main/java/com/example/stablestable/ui/login/LoginScreen.kt
@@ -14,10 +14,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -27,7 +23,6 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.stablestable.components.CreateUserWindow
 
 /*
  * Login Screen
