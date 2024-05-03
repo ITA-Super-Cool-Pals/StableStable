7494cec0dd4b621727c3781100f3132b62568ecf
jose-tl <j.tonnesenlucas@gmail.com>
2024-05-03 13:15:21 +0200

    msg

--- app/src/main/java/com/example/stablestable/data/repositories/AccountService.kt
+++ app/src/main/java/com/example/stablestable/data/repositories/AccountService.kt
@@ -4,8 +4,10 @@ import com.example.stablestable.data.classes.UserProfile
 
 interface AccountService {
     val currentUserId: String
+
     suspend fun getUser(userId:String): UserProfile?
     suspend fun getCurrentUser(): UserProfile?
     suspend fun createUser(user:UserProfile,pwd:String)
     suspend fun login(email:String,pwd:String)
+
 }
