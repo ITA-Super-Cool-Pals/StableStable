10bd87c3ec8f73bdb582f762d0fbc58e491bf633
SushiGirrl <lykkeflor@gmail.com>
2024-05-08 13:24:21 +0200

    - Crated Layout directory and notification.xml file

--- app/src/main/java/com/example/stablestable/data/repositories/impl/notificationsFCM/FCMNotificationService.kt
+++ app/src/main/java/com/example/stablestable/data/repositories/impl/notificationsFCM/FCMNotificationService.kt
@@ -1,4 +1,4 @@
-package com.example.stablestable.data.repositories.impl.NotificationsFCM
+package com.example.stablestable.data.repositories.impl.notificationsFCM
 
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
