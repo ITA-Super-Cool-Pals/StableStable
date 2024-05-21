19caa1d4eb2410f71bd2dd09a0fa4f25afe9eacb
jose-tl <j.tonnesenlucas@gmail.com>
2024-05-21 12:52:45 +0200

    init commit

---
+++ app/src/main/java/com/example/stablestable/components/ArrowBack.kt
@@ -0,0 +1,15 @@
+package com.example.stablestable.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun ArrowBack(){
+
+}
+
+@Preview
+@Composable
+fun ArrowBackPreview() {
+
+}
