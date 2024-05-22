dcd5dac9bd5596c55ad876073041ff090d5d1b0b
Emythiel <18901520+Emythiel@users.noreply.github.com>
2024-05-22 21:08:22 +0200

    Remove header text from horse and rider list (not really needed with the top bar present now)

--- app/src/main/java/com/example/stablestable/ui/stable/horses/StableHorsesScreenContent.kt
+++ app/src/main/java/com/example/stablestable/ui/stable/horses/StableHorsesScreenContent.kt
@@ -1,23 +1,14 @@
 package com.example.stablestable.ui.stable.horses
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 
 /*
@@ -37,27 +28,7 @@ fun StableHorsesScreenContent(
             .fillMaxSize()
             .padding(paddingValues)
     ) {
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(top = 15.dp, bottom = 20.dp)
-        ) {
-            Text(
-                text = "List of all horses in this stable",
-                fontSize = 24.sp,
-                fontWeight = FontWeight.Bold,
-                modifier = Modifier.padding(bottom = 10.dp)
-            )
-            Spacer(
-                modifier = Modifier
-                    .fillMaxWidth(0.9f)
-                    .height(1.dp)
-                    .background(Color.Gray)
-            )
-        }
-
-        // Column for displaying the list of horses
+        // Lazy Column for displaying the list of horses
         LazyColumn(
             contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp)
         ) {
